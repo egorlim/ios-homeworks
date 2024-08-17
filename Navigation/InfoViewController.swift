60f80873c637e8fa921ed21efa7979de68b354a7
egorlim <limarevegor6@gmail.com>
2024-08-17 18:35:50 +0300

    Add InfoViewController

---
+++ Navigation/InfoViewController.swift
@@ -0,0 +1,41 @@
+//
+//  InfoViewController.swift
+//  Navigation
+//
+//  Created by Лимарев Егор on 17.08.2024.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Info"
+
+        let showAlertButton = UIButton(type: .system)
+        showAlertButton.setTitle("Show Alert", for: .normal)
+        showAlertButton.addTarget(self, action: #selector(showAlert), for: .touchUpInside)
+
+        showAlertButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(showAlertButton)
+
+        NSLayoutConstraint.activate([
+            showAlertButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            showAlertButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+
+    @objc func showAlert() {
+        let alert = UIAlertController(title: "Alert", message: "This is an alert message.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+            print("OK Pressed")
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { _ in
+            print("Cancel Pressed")
+        }))
+        present(alert, animated: true, completion: nil)
+    }
+}
+
